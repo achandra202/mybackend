43c9cbdd2d4d2f7343102dfceabb0e43fb18ae3b
achandra202 <gajab202@gmail.com>
2025-08-13 15:51:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = process.env.MY_PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('<a href="https://www.w3schools.com">This is a link</a>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//console.log(process.env)
+
